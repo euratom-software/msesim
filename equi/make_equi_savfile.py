2a8ace8a93a01db3468fd14e79c79f9376630793
Gibson, Sam <sam.gibson@ccfe.ac.uk>
2018-12-06 11:34:52 +0000

    Added python scripts to calculate the magnetic field from an old hdf5 equilibrium file and write the values to a sav file structure for msesim. Fixed a bug where it was reading the values of psi(R,Z) instead of normalised flux coordinates for fluxcoord.

--- equi/make_equi_savfile.py
+++ equi/make_equi_savfile.py
@@ -1,15 +1,15 @@
 from equi.calculate_bfieldhdf5 import equilibrium
 import idlbridge as idl
 import numpy as np
-from scipy.io import readsav
+import matplotlib.pyplot as plt
 
 #Load a local HDF5 equilibrium file, and specify the timeslice
 
 efit = equilibrium(specified_time=0.365, efit_filepath='/work/sgibson/msesim/equi/efitOut.hdf5')
 
 #Gather the relevant variables that msesim wants
-R = efit.R
-Z = efit.Z
+R = efit.R[:,0]
+Z = efit.Z[0,:]
 nR = len(R)
 nZ = len(Z)
 
@@ -20,9 +20,16 @@ Bfld[0,:,:] = efit.B_R
 Bfld[1,:,:] = efit.B_Z
 Bfld[2,:,:] = efit.B_phi
 
-#Get magnetic flux co-ordinates and radial co-ordinate of magnetic axis
-fluxcoord = efit.psi_2d
+#Get normalised magnetic flux co-ordinates and radial co-ordinate of magnetic axis
+fluxcoord = efit.psi_n
 Rm = np.array([efit.r_magaxis])
+
+rr,zz = np.meshgrid(R,Z, indexing='ij')
+
+plt.figure()
+plt.contour(rr,zz,fluxcoord)
+plt.colorbar()
+plt.show()
 
 #Need to put our variables into idl using the idlbridge:
 idl.put('R', R)
@@ -30,12 +37,5 @@ idl.put('Z', Z)
 idl.put('Bfld', Bfld)
 idl.put('fluxcoord', fluxcoord)
 idl.put('Rm', Rm)
-
-#Get them back, and execute the save function to save these variables in a sav file format
-idl.get('R')
-idl.get('Z')
-idl.get('Bfld')
-idl.get('fluxcoord')
-idl.get('Rm')
 
 idl.execute("save, R, Z, Bfld, fluxcoord, Rm, filename='equi_MAST_24409_t0.365s.sav'")
