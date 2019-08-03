36e6a6b6fb7fc02e152c8146c29e3ee86eae4658
sgibson-mse <sam.gibson@durham.ac.uk>
2019-08-03 22:20:15 +0100

    removing unwanted files

--- msesim_submitscript.cmd
+++
@@ -1,8 +0,0 @@
-# @ executable = /work/sgibson/msesim/msesim_runscript
-# @ input = /dev/null
-# @ error = /work/sgibson/msesim/runs/imse_2d_MASTU_1MA_f73mm/job.err
-# @ output = /work/sgibson/msesim/runs/imse_2d_MASTU_1MA_f73mm/job.out
-# @ initialdir = /work/sgibson/msesim/
-# @ notify_user = sgibson
-# @ notification = complete
-# @ queue
